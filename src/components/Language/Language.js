063ac8b17da55942c0a6db333f9ad9d87a9e77f5
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-11 07:14:24 +0600

    create btn and style btn

--- src/components/Language/Language.js
+++ src/components/Language/Language.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Language.css'
+import { ArrowLongRightIcon } from '@heroicons/react/24/solid'
 const Language = ({ language }) => {
     console.log(language);
     const { name, logo } = language;
@@ -8,7 +9,11 @@ const Language = ({ language }) => {
             <img src={logo} alt="" />
             <div className='language-info'>
                 <h4>{name}</h4>
-                <button className='quiz-btn'>Quiz</button>
+                <div className='btn'>
+                    <button className='quiz-btn'>Quiz</button>
+                    <ArrowLongRightIcon  width="75" height="35" />
+                </div>
+
             </div>
         </div>
     );
