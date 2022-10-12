66c1f214052ecd75c7abe5cc0dbf68149305a6cc
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-12 08:27:20 +0600

    create _redirects file for netlify 404 not found page resolved

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './Rechart.css'
 
 const Services = () => {
@@ -8,12 +8,12 @@ const Services = () => {
     const data = datas.data;
     console.log(data);
     return (
-        <div className='rechart'>
-            <LineChart width={500} height={500} data={data}>
+        <div className='recharts'>
+           <LineChart width={400} height={400} data={data} className="rechart">
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
-                {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                 <Tooltip></Tooltip>
             </LineChart>
         </div>
